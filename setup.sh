b2f9588addcb97746391870cbde245b3386960f4
Ethan van Ballegooyen <ethanvb55@gmail.com>
2019-08-23 16:53:17 +0200

    Updates

---
+++ setup.sh
@@ -0,0 +1,22 @@
+rm -r apps/block_scout_web/priv/static
+
+mix do deps.get, local.rebar --force, deps.compile, compile
+
+cd apps/block_scout_web/assets; npm install && node_modules/webpack/bin/webpack.js --mode production; cd -
+
+cd apps/explorer && npm install; cd -
+
+cd apps/block_scout_web
+
+mix phx.gen.cert blockscout blockscout.local; cd -
+
+export 'COIN=ETH'
+export 'ETHEREUM_JSONRPC_VARIANT=geth'
+export 'ETHEREUM_JSONRPC_HTTP_URL=http://167.86.95.197:8547'
+export 'ETHEREUM_JSONRPC_WS_URL=ws://167.86.95.197:8548'
+export "BLOCKSCOUT_VERSION=V1.0.0 - Xerom"
+export 'LINK_TO_OTHER_EXPLORERS=false'
+export 'RELEASE_LINK=https://github.com/poanetwork/blockscout/releases/tag/${BLOCKSCOUT_VERSION}  '
+export 'SUBNETWORK=XERO'
+
+mix phx.server

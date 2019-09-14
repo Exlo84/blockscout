baf1a36a668053b66ab9bc17ca4e855967b315f3
Ethan van Ballegooyen <ethanvb55@gmail.com>
2019-09-14 16:19:22 +0200

    Added Announcements

--- setup.sh
+++ setup.sh
@@ -11,15 +11,36 @@ cd apps/explorer && npm install; cd -
 cd apps/block_scout_web; cd -
 
 export 'COIN=POA'
+echo 'COIN=POA'
+
 export 'ETHEREUM_JSONRPC_VARIANT=geth'
+echo 'ETHEREUM_JSONRPC_VARIANT=geth'
+
 export 'ETHEREUM_JSONRPC_HTTP_URL=http://167.86.95.197:8547'
+echo 'ETHEREUM_JSONRPC_HTTP_URL=http://167.86.95.197:8547'
+
 export 'ETHEREUM_JSONRPC_WS_URL=ws://167.86.95.197:8548'
-export "BLOCKSCOUT_VERSION=V1.0.0 - Xerom"
+echo 'ETHEREUM_JSONRPC_WS_URL=ws://167.86.95.197:8548'
+
+export "BLOCKSCOUT_VERSION=V1.1.0 - Xerom"
+echo "BLOCKSCOUT_VERSION=V1.1.0 - Xerom"
+
 export 'LINK_TO_OTHER_EXPLORERS=false'
+echo 'LINK_TO_OTHER_EXPLORERS=false'
+
 export 'RELEASE_LINK=https://github.com/poanetwork/blockscout/releases/tag/${BLOCKSCOUT_VERSION}  '
+echo 'RELEASE_LINK=https://github.com/poanetwork/blockscout/releases/tag/${BLOCKSCOUT_VERSION}  '
+
 export 'SUBNETWORK=XERO'
+echo 'SUBNETWORK=XERO'
+
 export 'NETWORK=Mainnet'
+echo 'NETWORK=Mainnet'
+
 export 'ADDRESS_WITH_BALANCES_UPDATE_INTERVAL=3 * 6'
+echo 'ADDRESS_WITH_BALANCES_UPDATE_INTERVAL=3 * 6'
+
 export 'TXS_COUNT_CACHE_PERIOD=6 * 6 * 2'
+echo 'TXS_COUNT_CACHE_PERIOD=6 * 6 * 2'
 
 mix phx.server

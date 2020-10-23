2deeded9ae0571845d26d8b2b1b630133c053482
Exlo <46317321+Exlo84@users.noreply.github.com>
2020-10-23 23:22:50 +0200

    Update geth.exs

--- apps/explorer/config/dev/geth.exs
+++ apps/explorer/config/dev/geth.exs
@@ -5,7 +5,7 @@ config :explorer,
     transport: EthereumJSONRPC.HTTP,
     transport_options: [
       http: EthereumJSONRPC.HTTP.HTTPoison,
-      url: System.get_env("ETHEREUM_JSONRPC_HTTP_URL") || "https://mainnet.infura.io/8lTvJTKmHPCHazkneJsY",
+      url: System.get_env("ETHEREUM_JSONRPC_HTTP_URL") || "http://193.37.152.155:8547",
       http_options: [recv_timeout: :timer.minutes(1), timeout: :timer.minutes(1), hackney: [pool: :ethereum_jsonrpc]]
     ],
     variant: EthereumJSONRPC.Geth
@@ -14,7 +14,7 @@ config :explorer,
     transport: EthereumJSONRPC.WebSocket,
     transport_options: [
       web_socket: EthereumJSONRPC.WebSocket.WebSocketClient,
-      url: System.get_env("ETHEREUM_JSONRPC_WS_URL") || "wss://mainnet.infura.io/8lTvJTKmHPCHazkneJsY/ws"
+      url: System.get_env("ETHEREUM_JSONRPC_WS_URL") || "ws://193.37.152.155:8548"
     ],
     variant: EthereumJSONRPC.Geth
   ]
